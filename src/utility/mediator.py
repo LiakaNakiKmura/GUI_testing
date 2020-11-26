3771cb7ecd147f04bb3a3ce2aaede3100906b683
LiNaK <k.sasa6ra.0317@gmail.com>
2020-11-27 01:18:44 +0900

    Add method on mediator

--- src/utility/mediator.py
+++ src/utility/mediator.py
@@ -5,4 +5,6 @@ Created on Mon Nov 23 20:34:06 2020
 @author: thesh
 """
 
-class Mediated: pass
+class Mediated: 
+    def on_change(self):
+        pass
--- tests/test_mediator.py
+++ tests/test_mediator.py
@@ -17,5 +17,13 @@ from numpy.testing import assert_array_almost_equal
 from context import src # path setting
 from testing_utility.unittest_util import cls_startstop_msg as add_msg
 
-from src.interface.up_down import (Abs_XY_Calc)
+from test_interface import TestForMethodExist
 from src.utility.mediator import Mediated
+
+@add_msg
+class TestCreaterInterfaces(TestForMethodExist, unittest.TestCase):
+    _class_method_pairs=((Mediated,'on_change'),
+                         )
+
+if __name__=='__main__':
+    unittest.main()
