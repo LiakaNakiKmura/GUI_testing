1bf242ca45ced1c568b78d97ed7c0b77b99019ab
LiNaK <k.sasa6ra.0317@gmail.com>
2020-12-13 21:02:36 +0900

    Add test on relations of mediators

--- src/utility/mediator.py
+++ src/utility/mediator.py
@@ -12,16 +12,35 @@ import abc
 # Original module  
 
 class Mediator(object):
-    def on_change(self):
-        pass
+    def __init__(self):
+        self._pairs = {}
+    
+    def on_change(self, caller, *args, **kargs):
+        print('Mediator Called')
+        callee = self._pairs[caller]
+        print(callee)
+        callee.on_change(*args, **kargs)
+    
+    def add_callee_caller_pairs(self, callee : 'Callee', caller : 'Caller')\
+        -> None:
+        if not isinstance(callee, Callee):
+            raise TypeError('callee must be instance of Callee')
+        elif not isinstance(caller, Caller):
+            raise TypeError('caller must be instance of Caller')
+        
+        self._pairs[caller] = callee
+        caller.mediator = self         
 
 class Caller(metaclass = abc.ABCMeta): 
     def __init__(self):
         self._mediator = None
         # Public variables
         
-    def on_change(self) -> None:
-        pass
+    def on_change(self, *args, **kargs) -> None:
+        # Call mediator to pass the data to callee.
+        
+        if self._mediator is not None:
+            self._mediator.on_change(self, *args, **kargs)
     
     @property
     def mediator(self) -> Mediator:
@@ -35,10 +54,5 @@ class Caller(metaclass = abc.ABCMeta):
 
     
 class Callee(metaclass = abc.ABCMeta): 
-    def on_change(self):
+    def on_change(self, *args, **kargs):
         pass
-    
-class Foo:
-    @property
-    def x(self, var):
-        assert ()
--- tests/test_mediator.py
+++ tests/test_mediator.py
@@ -24,23 +24,32 @@ from src.utility.mediator import (Caller, Mediator, Callee)
 class TestMediatorInterfaces(TestForMethodExist, unittest.TestCase):
     _class_method_pairs=((Caller,'on_change'),
                          (Mediator, 'on_change'),
+                         (Mediator, 'add_callee_caller_pairs'),
                          (Callee, 'on_change')
                          )
     _class_instanceattr_pairs = ((Caller, 'mediator'), 
                          )
 
 class MockCallee(Callee):
-    def on_change(self, *args, **kwargs):
-        self.ca(*args,**kwargs)
+    def on_change(self, *args, **kargs):
+        print('Callee Called')
+        self.args = args
+        self.kargs = kargs
 
 
+class MockCaller(Caller):
+    def on_change(self, *args, **kargs):
+        print('Caller Called')
+        super().on_change(*args, **kargs)
+        
 # Test that if Caller call on change, colleage obj is called. 
 
 @add_msg
 class TestMediator(unittest.TestCase):
     def setUp(self):
         self.mediator = Mediator()
-        self.caller = Caller()
+        self.caller = MockCaller()
+        self.callee = MockCallee()
     
     def test_validate_mediator_setup(self):
         bad_data = [0, 'a', [], {1:2}, None, lambda x: 0 ]
@@ -48,6 +57,38 @@ class TestMediator(unittest.TestCase):
             with self.assertRaises(TypeError):
                 self.caller.mediator=d
         self.caller.mediator = self.mediator
+    
+    def test_mediator_set_pairs(self):
+        '''
+        Test validation on add_callee_caller_pairs
+        '''
+        
+        class Foo: pass
+        f = Foo()
+        bad_pairs = ((f,self.caller), (self.callee, f))
+        for d in bad_pairs:
+            with self.assertRaises(TypeError):
+                self.mediator.add_callee_caller_pairs(*d)
+        self.mediator.add_callee_caller_pairs(self.callee, self.caller)
+        
+        # TODO: add test to same caller is not allowed:
+        
+    def test_mediator_relation(self):
+        '''
+        Test mediator relations.
+        Set up is just pass caller, callee pairs.
+        If Caller is on_change with args, kargs, Callee is on_change with args,
+        kargs.        
+        '''
+        self.mediator.add_callee_caller_pairs(self.callee, self.caller)
+        input_data = tuple(range(20))
+        self.caller.on_change(*input_data)
+        self.assertEqual(self.callee.args, input_data)
+        
+        input_data2 = {chr(65+i):i for i in range(20)}
+        self.caller.on_change(**input_data2)
+        self.assertEqual(self.callee.kargs, input_data2)
+        
     
 if __name__=='__main__':
     unittest.main()
